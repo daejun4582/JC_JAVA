b09100f5c66bbd99a170a306d8eb370d11cc5ca1
daejun4582 <qkseowns9206@gmail.com>
2023-03-02 14:29:33 +0900

    lab of l02

---
+++ lab/src/lab02/Pencil.java
@@ -0,0 +1,10 @@
+package lab02;
+
+public class Pencil {
+    private int roundness;
+    private int thickness;
+    private String color = "red";
+
+    public void write(){}
+    public void erase(){}
+}
---
+++ lab/src/lab02/SmartPencil.java
@@ -0,0 +1,12 @@
+package lab02;
+
+public class SmartPencil extends Pencil{
+
+    private boolean isMagnetic;
+    private int batteryLevel;
+    public SmartPencil(){
+        super();
+    }
+    public void getBatteryLevel(){}
+    public void connect(){}
+}
